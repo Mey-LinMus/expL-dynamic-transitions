f020e80c4cd7593a21b84ecd9c10132a2583afc3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-10-09 11:22:03 +0200

    Adding spiderman image

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -1,5 +1,12 @@
-function SectionOne() {
-  return <div></div>;
-}
+import React from "react";
+import Spiderman from "../assets/spiderman.png";
+
+const SectionOne = () => {
+  return (
+    <div>
+      <img src={Spiderman} alt="Spideman" />
+    </div>
+  );
+};
 
 export default SectionOne;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SectionOne from "../components/SectionOne";
+import "../styles/home.css"
+
+const Home = () => {
+  return (
+    <header>
+      <SectionOne />
+    </header>
+  );
+};
+
+export default Home;
