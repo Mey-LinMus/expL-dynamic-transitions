3a05e3d3f558597db8d3f6759837ba25a8259e95
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-10-17 17:00:45 +0200

    Experimenting with horizontal scroll

--- src/components/SectionTree.jsx
+++ src/components/SectionTree.jsx
@@ -6,6 +6,7 @@ import TomHollandImage from "../assets/actors/TomHolland.jpg";
 import NewImage1 from "../assets/actors/TobeySpiderman.jpg";
 import NewImage2 from "../assets/actors/AndrewSpiderman.jpg";
 import NewImage3 from "../assets/actors/TomSpiderman.jpg";
+import "../styles/section.css";
 
 const images = [
   { mainSrc: TobeyMaguireImage, hoverSrc: NewImage1, alt: "Tobey Maguire" },
@@ -16,12 +17,34 @@ const images = [
 const variants = {
   visible: { opacity: 1, x: 0 },
   hidden: { opacity: 0, x: -100 },
-  hover: { hover: 1 }, 
+  hover: { hover: 1 },
 };
 
 const SectionTree = () => {
-  const [hoveredImages, setHoveredImages] = useState(Array(images.length).fill(false));
-  const [showHoverImages, setShowHoverImages] = useState(Array(images.length).fill(false));
+  const [hoveredImages, setHoveredImages] = useState(
+    Array(images.length).fill(false)
+  );
+  const [showHoverImages, setShowHoverImages] = useState(
+    Array(images.length).fill(false)
+  );
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = 100; // Adjust as needed
+      if (window.scrollY > scrollThreshold) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleMouseEnter = (index) => {
     setHoveredImages((prev) => {
@@ -49,19 +72,13 @@ const SectionTree = () => {
         updatedShowHoverImages[index] = false;
         return updatedShowHoverImages;
       });
-    }, 100); 
+    }, 100);
   };
 
-  useEffect(() => {
-
-    return () => {
-      setHoveredImages(Array(images.length).fill(false));
-      setShowHoverImages(Array(images.length).fill(false));
-    };
-  }, []);
-
   return (
-    <div className="horizontal">
+    <div
+      className={`horizontal section-three ${isFixed ? "fixed-section" : ""}`}
+    >
       <AnimatePresence>
         {images.map((image, index) => (
           <motion.div
@@ -71,7 +88,7 @@ const SectionTree = () => {
             exit="hidden"
             variants={variants}
             transition={{ duration: 1 }}
-            style={{ marginRight: "20px" }}
+            style={{ marginRight: "80px" }}
             onMouseEnter={() => handleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave(index)}
           >
@@ -79,8 +96,10 @@ const SectionTree = () => {
             <motion.img
               src={hoveredImages[index] ? image.hoverSrc : image.mainSrc}
               alt={image.alt}
-              className={`horizontal-img ${showHoverImages[index] ? "show-hover" : ""}`}
-              whileHover="hover" 
+              className={`horizontal-img ${
+                showHoverImages[index] ? "show-hover" : ""
+              }`}
+              whileHover="hover"
               variants={variants}
             />
           </motion.div>
