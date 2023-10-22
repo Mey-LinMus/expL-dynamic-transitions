36c2d0715e254c9ef4428bc4689319f3d1967ebc
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2023-10-22 13:15:07 +0200

    Adding svg animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import SectionOne from "../components/SectionOne";
 import SectionTwo from "../components/SectionTwo";
 import SectionThree from "../components/SectionThree";
-import BackgroundMusic from "../components/BackgroundMusic";
 import SectionFour from "../components/SectionFour";
+import SectionFive from "../components/SectionFive";
 import CustomCursor from "../components/CustomCursor";
+import BackgroundMusic from "../components/BackgroundMusic";
 import ParticleBackground from "../components/Particles"; 
 import "../styles/home.css";
 
@@ -13,11 +14,12 @@ const Home = () => {
     <header>
       <CustomCursor />
       <ParticleBackground />
-      <BackgroundMusic />
+      {/* <BackgroundMusic /> */}
       <SectionOne />
       <SectionTwo />
       <SectionThree />
       <SectionFour />
+      <SectionFive/>
     </header>
   );
 };
